06e87c6d58c9c796196043d597d5d3dc04558e3a
GarciaEzequiel <ezegarciaies@gmail.com>
2025-04-15 17:49:24 -0300

    15/04/2025 - prueba

--- Pacientes (Estruct. Cola)/frmSiguiente.Designer.cs
+++ Pacientes (Estruct. Cola)/frmSiguiente.Designer.cs
@@ -95,7 +95,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(360, 180);
+            this.ClientSize = new System.Drawing.Size(360, 189);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
